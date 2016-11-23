128e668795cdbaffd7cf3d489d13d92c5c005e36
Alejandro Gutiérrez Novoa <agnovoa2@esei.uvigo.es>
2016-11-23 16:20:15 +0100

    Added more CRUD test for Item
    Changed database from persistence.xml and SQLBasedTest class.

--- src/test/java/wow/proyectosi/ItemTest.java
+++ src/test/java/wow/proyectosi/ItemTest.java
@@ -6,6 +6,7 @@ import static wow.proyectosi.TransactionUtils.doTransaction;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -36,6 +37,7 @@ public class ItemTest extends SQLBasedTest{
 		statement.executeUpdate(
 				"Delete From Item Where id = 23416");
 	}
+	//C
 	@Test
 	public void testCreateItem() throws SQLException {
 		
@@ -62,6 +64,7 @@ public class ItemTest extends SQLBasedTest{
 		assertEquals(1, rs.getInt("total"));
 
 	}
+	//R
 	@Test
 	public void testFindItemById() throws SQLException{
 		//prepare database for test
@@ -80,5 +83,109 @@ public class ItemTest extends SQLBasedTest{
 		
 		statement.executeUpdate(
 				"Delete From Item Where id = " + i.getId());
+	}
+	//U
+	@Test
+	public void testUpdateItem() throws SQLException{
+		//prepare database for test
+		Statement statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"Insert Into Item(id,itemLevel,name,sellPrice,slot,type) values(23416,80,'Corrupted Ashbringer',110631,'Main Hand','Two-Hand Sword')"
+				);
+		
+		int id = 23416;
+		
+		doTransaction(emf, em -> {
+			Item i = em.find(Item.class, id);
+			i.setName("Ashbringer");
+		});
+		
+		//check
+		statement = jdbcConnection.createStatement();
+		ResultSet rs = statement.executeQuery(
+				"SELECT * FROM Item WHERE id = "+id);
+		rs.next();
+		
+		assertEquals("Ashbringer", rs.getString("name"));
+		assertEquals(id, rs.getInt("id"));
+	}
+	
+	//U
+	private Item aDetachedItem = null;
+	@Test
+	public void testUpdateByMerge() throws SQLException {
+		//prepare database for test
+		Statement statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"Insert Into Item(id,itemLevel,name,sellPrice,slot,type) values(23416,80,'Corrupted Ashbringer',110631,'Main Hand','Two-Hand Sword')"
+				);
+		int id = 23416;
+		
+		doTransaction(emf, em -> {
+			aDetachedItem = em.find(Item.class, id);
+		});
+		// e is detached, because the entitymanager em is closed (see doTransaction)
+		
+		aDetachedItem.setName("Ashbringer");
+		
+		doTransaction(emf, em -> {
+			em.merge(aDetachedItem);
+		});
+		
+		//check
+		statement = jdbcConnection.createStatement();
+		ResultSet rs = statement.executeQuery(
+				"SELECT * FROM Item WHERE id = "+id);
+		rs.next();
+		
+		assertEquals("Ashbringer", rs.getString("name"));
+		assertEquals(id, rs.getInt("id"));
+	}
+	
+	//D
+	@Test
+	public void testDeleteItem() throws SQLException {
+		//prepare database for test
+		Statement statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"Insert Into Item(id,itemLevel,name,sellPrice,slot,type) values(23416,80,'Corrupted Ashbringer',110631,'Main Hand','Two-Hand Sword')"
+				);
+		int id = 23416;
+		
+		doTransaction(emf, em -> {
+			Item i = em.find(Item.class, id);
+			em.remove(i);
+		});
+		
+		//check
+		statement = jdbcConnection.createStatement();
+		ResultSet rs = statement.executeQuery(
+				"SELECT COUNT(*) as total FROM Item WHERE id = "+id);
+		rs.next();
+		
+		assertEquals(0, rs.getInt("total"));
+	}
+	
+	//L
+	@Test
+	public void testListBosses() throws SQLException {
+		//prepare database for test
+		Statement statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"Insert Into Item(id,itemLevel,name,sellPrice,slot,type) values(23416,80,'Corrupted Ashbringer',110631,'Main Hand','Two-Hand Sword')",Statement.RETURN_GENERATED_KEYS
+				);
+		//prepare database for test
+		statement.executeUpdate(
+				"Insert Into Item(id,itemLevel,name,sellPrice,slot,type) values(23417,80,'Ashbringer',110631,'Main Hand','Two-Hand Sword')",Statement.RETURN_GENERATED_KEYS
+				);
+		
+		List<Item> items = emf.createEntityManager()
+			.createQuery("SELECT i FROM Item i ORDER BY i.name", Item.class)
+			.getResultList();
+		
+		//check
+		assertEquals(2, items.size());
+		assertEquals("Ashbringer", items.get(0).getName());
+		assertEquals("Corrupted Ashbringer", items.get(1).getName());
 	}
 }
