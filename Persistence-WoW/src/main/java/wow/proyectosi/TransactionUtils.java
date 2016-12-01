6457529c8b533992c9df6cf6294dd8c28ce77313
Alejandro Gutiérrez Novoa <agnovoa2@esei.uvigo.es>
2016-12-01 16:35:19 +0100

    You can now list and create Bosses and Items.
    Added create and list zul for item and boss.
    Added viewmodel for item and boss.

--- Persistence-WoW/src/main/java/wow/proyectosi/TransactionUtils.java
+++ Persistence-WoW/src/main/java/wow/proyectosi/TransactionUtils.java
@@ -8,17 +8,27 @@ public class TransactionUtils {
 
 	public static void doTransaction(EntityManagerFactory emf, Transaction transaction) {
 		EntityTransaction tx = null;
-		EntityManager em = emf.createEntityManager();												
-		try{
-			tx = em.getTransaction();			
-			try{
-				tx.begin();		
+		EntityManager em = emf.createEntityManager();
+		try {
+			tx = em.getTransaction();
+			try {
+				tx.begin();
 					transaction.run(em);
-				tx.commit();						
-			}finally{
+				tx.commit();
+			} finally{
 				if (tx !=null && tx.isActive()){ tx.rollback(); }
 			}
-		}finally{	em.close(); 	}
+		} finally{	em.close(); 	}
 	}
-	
+
+	public static void doTransaction(EntityManager em, Transaction transaction) {
+		EntityTransaction tx = em.getTransaction();
+		try {
+			tx.begin();
+				transaction.run(em);
+			tx.commit();
+		} finally {
+			if (tx !=null && tx.isActive()){ tx.rollback(); }
+		}
+	}
 }
